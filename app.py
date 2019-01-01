a26afcd3c3ed0afc2f2d764c56462a1f44d0c18b
bjarkemoensted <bjarkemoensted@gmail.com>
2019-01-01 21:30:46 +0100

    started stuff

---
+++ app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 26 15:27:24 2018
+
+@author: ahura
+"""
+
+import datetime
+from flask import Flask, render_template, flash, request, session, redirect
+import urllib.request
+
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = 'DGD463463etrhSDGHSdfhsd3t34ERGSDFg'
+
+def get_bin():
+    d = {49: 1, 48: 0}
+    x = urllib.request.urlopen("https://qrng.anu.edu.au/ran_bin.php").read()
+    int_ = d[list(x)[0]]
+    return int_
+
+@app.route("/")
+def index():
+
+    r = get_bin()
+
+    if r == 0:
+        return "tails"
+    else:
+        return "heads"
+
+
+if __name__ == "__main__":
+    app.run()
